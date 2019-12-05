8d00bda116efbae3f80b399d688590bac724a244
Helington <helington007@hotmail.com>
2019-12-05 15:29:03 -0300

    website_backupdatabase_laravel

--- bkp.bat
+++ bkp.bat
@@ -1,1 +1,5 @@
+REM Descobri guerreiro o "REM" é utilizado para comentar...
+
+REM php C:\xampp\htdocs\semad\website_backupdatabase_laravel\backup.php
+
 php C:\laragon\www\website_backupdatabase_laravel\backup.php
